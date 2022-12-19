e5dabf1af8af287125b75d946254fe470537d827
Zone Media <112625614+zonemedia@users.noreply.github.com>
2022-12-20 06:32:33 +0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-if(document.domain == 'zonemedia.github.io'){
-window.location.href = 'https://zonemedia.github.io';
-} else if(document.domain == 'defacehtml.web.app'){
-alert("Jangan salahkan kami jika terdapat wajahmu tampil di halaman depan website!");
-} else if(document.domain == 'example.com'){
-window.location.href = 'https://zonemedia.github.io';
-}
